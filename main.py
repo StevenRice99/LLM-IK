767cc65bbcde91e939eae0de8d747b75976705ad
StevenRice99 <Contact@StevenRice.ca>
2024-05-21 20:52:40 -0400

    trying things

--- main.py
+++ main.py
@@ -4,62 +4,60 @@ import random
 import mujoco
 import mujoco.viewer
 import numpy as np
+import dm_control.utils.inverse_kinematics
 import os
+from dm_control import mujoco as mujoco_dm
 
 
 def load_model(path):
     model = mujoco.MjModel.from_xml_path(path)
-    data = mujoco.MjData(model)
-    return model, data
-
-
-def get_joint_limits(model):
-    #joint_names = []
     lower = []
     upper = []
     for i in range(model.njnt):
-        #joint_names.append(mujoco.mj_id2name(model, mujoco.mjtObj.mjOBJ_JOINT, i))
         if model.jnt_limited[i]:
             lower.append(model.jnt_range[i][0])
             upper.append(model.jnt_range[i][1])
         else:
             lower.append(float('-inf'))
             upper.append(float('inf'))
-    return lower, upper
+    return model, mujoco.MjData(model), lower, upper, mujoco.mj_id2name(model, mujoco.mjtObj.mjOBJ_SITE, model.nsite - 1)
 
 
-def mid_positions(model, data, lower, upper, stopping: float = 0.00000001):
-    joint_positions = np.zeros(model.njnt)
+def mid_positions(model, data, lower, upper):
+    values = np.zeros(model.njnt)
     for i in range(model.njnt):
-        joint_positions[i] = joint_positions[i] = (lower[i] + upper[i]) / 2
-    return set_joint_positions(model, data, joint_positions, stopping)
+        values[i] = values[i] = (lower[i] + upper[i]) / 2
+    return set_joints(model, data, values)
 
 
-def random_positions(model, data, lower, upper, stopping: float = 0.00000001):
-    joint_positions = np.zeros(model.njnt)
+def random_positions(model, data, lower, upper):
+    values = np.zeros(model.njnt)
     for i in range(model.njnt):
-        joint_positions[i] = random.uniform(lower[i], upper[i])
-    return set_joint_positions(model, data, joint_positions, stopping)
+        values[i] = random.uniform(lower[i], upper[i])
+    return set_joints(model, data, values)
 
 
-def set_joint_positions(model, data, joint_positions, stopping: float = 0.00000001):
+def set_joints(model, data, values):
     for i in range(model.njnt):
-        data.ctrl[i] = joint_positions[i]
-        #data.qpos[i] = joint_positions[i]
+        data.ctrl[i] = values[i]
+        data.qpos[i] = values[i]
         data.qvel[i] = 0
     mujoco.mj_forward(model, data)
-    moving = True
-    while moving:
-        mujoco.mj_step(model, data)
-        moving = False
-        for i in range(model.njnt):
-            if data.qvel[i] > stopping:
-                moving = True
-                break
-    pos = data.xpos[model.nbody - 1] - data.xpos[0]
-    difference = quaternion_difference(data.xquat[0], data.xquat[model.nbody - 1])
-    angles = quaternion_to_euler(difference)
-    return pos, angles
+    mujoco.mj_step(model, data)
+
+
+def get_pose(model, data):
+    pos_ref = data.site_xpos[model.nsite - 1]
+    quat_ref = quaternion_difference(data.xquat[model.nbody - 1], model.site_quat[model.nsite - 1])
+    pos = [pos_ref[0], pos_ref[1], pos_ref[2]]
+    quat = [quat_ref[0], quat_ref[1], quat_ref[2], quat_ref[3]]
+    return pos, quat
+
+
+def get_pose_relative(model, data):
+    pos = data.site_xpos[model.nsite - 1] - data.xpos[0]
+    quat = quaternion_difference(data.xquat[0], data.site_quat[model.nsite - 1])
+    return pos, quat
 
 
 def quaternion_inverse(q):
@@ -101,41 +99,60 @@ def quaternion_to_euler(quaternion):
 
     # ZYX convention
     # Roll (x-axis rotation)
-    sinr_cosp = 2.0 * (w * x + y * z)
-    cosr_cosp = 1.0 - 2.0 * (x * x + y * y)
-    roll = math.atan2(sinr_cosp, cosr_cosp)
+    sin_roll_cos_pitch = 2.0 * (w * x + y * z)
+    cos_roll_cos_pitch = 1.0 - 2.0 * (x * x + y * y)
+    roll = math.atan2(sin_roll_cos_pitch, cos_roll_cos_pitch)
+
+    sin_pitch = 2.0 * (w * y - z * x)
 
     # Pitch (y-axis rotation)
-    sinp = 2.0 * (w * y - z * x)
-    if abs(sinp) >= 1:
-        pitch = math.copysign(math.pi / 2, sinp)  # Use 90 degrees if out of range
-    else:
-        pitch = math.asin(sinp)
+    # Use 90 degrees if out of range
+    pitch = math.copysign(math.pi / 2, sin_pitch) if abs(sin_pitch) >= 1 else math.asin(sin_pitch)
 
     # Yaw (z-axis rotation)
-    siny_cosp = 2.0 * (w * z + x * y)
-    cosy_cosp = 1.0 - 2.0 * (y * y + z * z)
-    yaw = math.atan2(siny_cosp, cosy_cosp)
+    sin_yaw_cos_pitch = 2.0 * (w * z + x * y)
+    cos_yaw_cos_pitch = 1.0 - 2.0 * (y * y + z * z)
+    yaw = math.atan2(sin_yaw_cos_pitch, cos_yaw_cos_pitch)
 
     return roll, pitch, yaw
+
+
+def view(model, data):
+    viewer = mujoco.viewer.launch_passive(model, data)
+    while viewer.is_running():
+        viewer.sync()
 
 
 def main():
     path = os.path.join(os.getcwd(), "models", "universal_robots_ur5e", "ur5e.xml")
 
-    model, data = load_model(path)
+    model, data, lower, upper, site = load_model(path)
 
-    lower, upper = get_joint_limits(model)
+    #pos, angles = mid_positions(model, data, lower, upper)
+    random_positions(model, data, lower, upper)
+    goal_pos, goal_quat = get_pose(model, data)
+    print(f"Goal = {goal_pos} | {quaternion_to_euler(goal_quat)}")
+    view(model, data)
 
-    #mid_positions(model, data, lower, upper)
-    pos, angles = random_positions(model, data, lower, upper)
-    print(f"Position Offset {pos}")
-    print(f"Rotation Offset {angles}")
+    random_positions(model, data, lower, upper)
+    temp_pos, temp_quat = get_pose(model, data)
+    #print(f"Random = {temp_pos} | {quaternion_to_euler(temp_quat)}")
 
-    viewer = mujoco.viewer.launch_passive(model, data)
+    physics = mujoco_dm.Physics.from_xml_path(path)
+    copy_pos = goal_pos.copy()
+    copy_quat = goal_quat.copy()
+    values = dm_control.utils.inverse_kinematics.qpos_from_site_pose(physics, site, copy_pos, copy_quat)
+    values = values.qpos
+    set_joints(model, data, values)
+    result_pos, result_quat = get_pose(model, data)
+    print(f"Result = {result_pos} | {quaternion_to_euler(result_quat)}")
 
-    while viewer.is_running():
-        viewer.sync()
+    euler_goal = quaternion_to_euler(goal_quat)
+    euler_result = quaternion_to_euler(result_quat)
+    diff_pos = np.setdiff1d(np.array(goal_pos), np.array(result_pos))
+    diff_euler = np.setdiff1d(np.array(euler_goal), np.array(euler_result))
+    print(f"Difference = {diff_pos} | {diff_euler}")
+    view(model, data)
 
 
 if __name__ == "__main__":
